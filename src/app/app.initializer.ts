4973bbe964075b208c2c756b62179a36406d902f
Manuel de Jesús González Arvelo <manuelarvelo19@gmail.com>
2023-09-17 21:27:14 +0200

    remove appInitializerFn file (#12)
    : cleanup
    
    Co-authored-by: manuel-unimedia <manuel.gonzalez.ext@iceservices.com>

--- src/app/app.initializer.ts
+++
@@ -1,7 +0,0 @@
-import { ConfigService } from "./core/services/config/config.service";
-
-export const appInitializerFn = (appConfig: ConfigService) => {
-  return () => {
-    return appConfig.loadAppConfig();
-  };
-};
